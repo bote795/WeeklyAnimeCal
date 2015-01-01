9a3c03f4efe8428cbd22c8c612f89c4f7e163c39
bote795 <nbotell2@gmail.com>
2015-01-01 17:02:12 -0600

    Table GUI added
    added general table for weekly GUI w/o any interaction yet

--- js/GUI.js
+++ js/GUI.js
@@ -1,3 +1,4 @@
+//takes a name and returns the html for a checkbox
 function CreateCheckbox(name)
 {
 	string=""; 
@@ -5,4 +6,41 @@ function CreateCheckbox(name)
 		"<input type='checkbox'> "+name+
 	"</label>";
 	return string;
+}
+//takes in a dictionary that contains the days of the weeks from 0-6
+//which each contains an array of animes for that day
+//retruns tables in html
+function CreateTableRows(DictAnimesPDay)
+{
+	TableRows="";
+	//find the biggest array in dict
+	MaxSize=-1;
+	for(key in DictAnimesPDay)
+	{
+		if(DictAnimesPDay[key].length>MaxSize)
+		{
+			MaxSize=DictAnimesPDay[key].length;
+		}
+	}
+	//loop that many times
+	for (var i = 6; i >= 0; i--) {
+		//create table row inside looping through 0-6 and adding the td
+		var TableRow="<tr>";
+		for(var e=0; e <=6; e++)
+		{
+			if ( DictAnimesPDay[e].length !=0) 
+			{
+				temp=DictAnimesPDay[e].pop()
+				console.log(temp);
+				TableRow+= "<td>"+ temp["name"] +"</td>"
+			}
+			else
+			{
+				TableRow+= "<td></td>"
+			}
+		}
+		TableRow+="</tr>";
+		TableRows+=TableRow;
+	}
+	return TableRows;
 }
